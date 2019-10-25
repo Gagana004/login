a476e9657425c84ba562db0b46f19243651fbcbb
Gagana Kavindu Gamage <37533941+Gagana004@users.noreply.github.com>
2019-10-25 08:47:58 +0530

    creating login with session handling

---
+++ application/models/main_model.php
@@ -0,0 +1,19 @@
+<?php 
+
+function can_login($username, $password)
+{
+	$this->db->where('username', $username);
+	$this->db->where('password', $password);
+	$query = $this->db->get('user');
+
+	if($query->num_rows() > 0)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+?>
